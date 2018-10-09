35002a91fc34b9aa9a4d1cd2eb16cbafd76a3276
Yurii Rochniak <yrochnyak@gmail.com>
2018-10-09 12:54:07 +0300

    Fix mistake in a function

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 locals {
   vpc_peering_id        = "${compact(concat(coalescelist(aws_vpc_peering_connection.this.*.id, aws_vpc_peering_connection.this_cross_region.*.id), list("")))}"
   peering_accept_status = "${compact(concat(coalescelist(aws_vpc_peering_connection.this.*.accept_status, aws_vpc_peering_connection.this_cross_region.*.accept_status), list("")))}"
-  private_route_tables  = "${compact(concat(coalescelist(var.private_route_table_ids), list("")))}"
-  public_route_tables   = "${compact(concat(coalescelist(var.private_route_table_ids), list("")))}"
+  private_route_tables  = "${compact(concat(var.private_route_table_ids, list("")))}"
+  public_route_tables   = "${compact(concat(var.private_route_table_ids, list("")))}"
 }
 
 output "vpc_peering_id" {
