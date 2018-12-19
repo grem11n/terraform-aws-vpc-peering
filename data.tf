6198950678254c57336e16579dca17a0ac92d6af
Yurii Rochniak <yrochnyak@gmail.com>
2018-12-19 21:20:31 +0200

    Fix formatting

--- data.tf
+++ data.tf
@@ -1,21 +1,21 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_vpc" "this_vpc" {
-  provider      = "aws.this"
-  id = "${var.this_vpc_id}"
+  provider = "aws.this"
+  id       = "${var.this_vpc_id}"
 }
 
 data "aws_vpc" "peer_vpc" {
-  provider      = "aws.peer"
-  id = "${var.peer_vpc_id}"
+  provider = "aws.peer"
+  id       = "${var.peer_vpc_id}"
 }
 
 data "aws_route_tables" "this_vpc_rts" {
-  provider      = "aws.this"
-  vpc_id = "${var.this_vpc_id}"
+  provider = "aws.this"
+  vpc_id   = "${var.this_vpc_id}"
 }
 
 data "aws_route_tables" "peer_vpc_rts" {
-  provider      = "aws.peer"
-  vpc_id = "${var.peer_vpc_id}"
+  provider = "aws.peer"
+  vpc_id   = "${var.peer_vpc_id}"
 }
