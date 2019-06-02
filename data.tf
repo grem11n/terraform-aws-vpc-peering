d0b83a7f7d0e6dbe070624f46a927b92289b8f20
Yurii Rochniak <yrochnyak@gmail.com>
2019-06-02 19:47:36 +0300

    Refactoring for Terraform 0.11

--- data.tf
+++ data.tf
@@ -1,4 +1,5 @@
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
 data "aws_vpc" "this_vpc" {
   provider = "aws.this"
--- variables.tf
+++ variables.tf
@@ -13,11 +13,6 @@ variable "this_vpc_id" {
   default     = ""
 }
 
-variable "cross_region_peering" {
-  description = "Is it a cross region peering: bool"
-  default     = false
-}
-
 variable "peer_region" {
   description = "Peer Region Name e.g. us-east-1: string"
   default     = ""
@@ -42,4 +37,34 @@ variable "tags" {
   description = "Tags: map"
   type        = "map"
   default     = {}
+}
+
+variable "peer_dns_resolution" {
+  description = "Indicates whether a local VPC can resolve public DNS hostnames to private IP addresses when queried from instances in a peer VPC"
+  default     = false
+}
+
+variable "peer_link_to_peer_classic" {
+  description = "Indicates whether a local ClassicLink connection can communicate with the peer VPC over the VPC Peering Connection"
+  default     = false
+}
+
+variable "peer_link_to_local_classic" {
+  description = "Indicates whether a local VPC can communicate with a ClassicLink connection in the peer VPC over the VPC Peering Connection"
+  default     = false
+}
+
+variable "this_dns_resolution" {
+  description = "Indicates whether a local VPC can resolve public DNS hostnames to private IP addresses when queried from instances in a this VPC"
+  default     = false
+}
+
+variable "this_link_to_peer_classic" {
+  description = "Indicates whether a local ClassicLink connection can communicate with the this VPC over the VPC Peering Connection"
+  default     = false
+}
+
+variable "this_link_to_local_classic" {
+  description = "Indicates whether a local VPC can communicate with a ClassicLink connection in the this VPC over the VPC Peering Connection"
+  default     = false
 }
