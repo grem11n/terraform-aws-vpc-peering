2ebcadcdb982f03b927973e433c326167f6b365e
Yurii Rochniak <yurii.rochniak@n26.com>
2019-11-14 15:04:40 +0100

    Fix fixtures for new vars

--- examples/single-account-multi-region/variables.tf
+++ examples/single-account-multi-region/variables.tf
@@ -3,3 +3,11 @@
 variable "this_vpc_id" {}
 
 variable "peer_vpc_id" {}
+
+variable "aws_this_access_key" {
+  description = "AWS Access Key for requester account"
+}
+
+variable "aws_this_secret_key" {
+  description = "AWS Secret Key for requester account"
+}
