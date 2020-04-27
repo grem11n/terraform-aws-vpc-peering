fdb31f7ec81e8d9cdf8ff944437ff6577610e221
Tomas Bäckman <50198527+tomasbackman@users.noreply.github.com>
2020-04-27 20:37:42 +0200

    Use subnets for peering (#1)
    * Some cleanup
    * Added functionality for limiting peering to subnets
    * Small code style fixes
    * Corrected dest_cidrs and set to sets

--- main.tf
+++ main.tf
@@ -14,6 +14,33 @@ locals {
   same_region            = data.aws_region.this.name == data.aws_region.peer.name
   same_account           = data.aws_caller_identity.this.account_id == data.aws_caller_identity.peer.account_id
   same_acount_and_region = local.same_region && local.same_account
+
+  // Rout table should either be the one for the vpc, or the ones associated to the subnets if subnets are given
+  this_rts_ids_new = data.aws_route_tables.this_vpc_rts.ids
+  peer_rts_ids_new = data.aws_route_tables.peer_vpc_rts.ids
+
+  this_rts_ids = length(var.this_subnets_ids) == 0 ? local.this_rts_ids_new : data.aws_route_table.this_subnet_rts[*].route_table_id
+  peer_rts_ids = length(var.peer_subnets_ids) == 0 ? local.peer_rts_ids_new : data.aws_route_table.peer_subnet_rts[*].route_table_id
+
+  // Destination cidrs for this are in peer and vice versa
+  this_dest_cidrs = length(var.peer_subnets_ids) == 0 ? toset([data.aws_vpc.peer_vpc.cidr_block]) : toset(data.aws_subnet.peer[*].cidr_block)
+  peer_dest_cidrs = length(var.this_subnets_ids) == 0 ? toset([data.aws_vpc.this_vpc.cidr_block]) : toset(data.aws_subnet.this[*].cidr_block)
+
+  // In each route table there should be 1 route for each subnet, so combining the two sets
+  this_routes = [
+    for pair in setproduct(local.this_rts_ids, local.this_dest_cidrs) : {
+      rts_id    = pair[0]
+      dest_cidr = pair[1]
+    }
+  ]
+
+  // In each route table there should be 1 route for each subnet, so combining the two sets
+  peer_routes = [
+    for pair in setproduct(local.peer_rts_ids, local.peer_dest_cidrs) : {
+      rts_id    = pair[0]
+      dest_cidr = pair[1]
+    }
+  ]
 }
 
 ##########################
@@ -64,23 +91,25 @@ resource "aws_vpc_peering_connection_options" "accepter" {
 }
 
 ###################
-# This VPC Routes #
+# This VPC Routes #  Route from THIS route table to PEER cidr
 ###################
-resource "aws_route" "this_routes_region" {
+resource "aws_route" "this_routes" {
   provider                  = aws.this
-  count                     = length(data.aws_route_tables.this_vpc_rts.ids)
-  route_table_id            = tolist(data.aws_route_tables.this_vpc_rts.ids)[count.index]
-  destination_cidr_block    = data.aws_vpc.peer_vpc.cidr_block
+  # Only create routes in this route table if input allows, and in that case for all combinations
+  count                     = var.from_this ? length(local.this_routes) : 0
+  route_table_id            = local.this_routes[count.index].rts_id
+  destination_cidr_block    = local.this_routes[count.index].dest_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
 
 ###################
-# Peer VPC Routes #
+# Peer VPC Routes #  Route from PEER route table to THIS cidr
 ###################
-resource "aws_route" "peer_routes_region" {
+resource "aws_route" "peer_routes" {
   provider                  = aws.peer
-  count                     = length(data.aws_route_tables.peer_vpc_rts.ids)
-  route_table_id            = tolist(data.aws_route_tables.peer_vpc_rts.ids)[count.index]
-  destination_cidr_block    = data.aws_vpc.this_vpc.cidr_block
+  # Only create routes in this route table if input allows, and in that case for all combinations
+  count                     = var.from_peer ? length(local.peer_routes) : 0
+  route_table_id            = local.peer_routes[count.index].rts_id
+  destination_cidr_block    = local.peer_routes[count.index].dest_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
