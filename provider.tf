1ee4c196eb72cc4674a9b7a07609ae5cb4d0f95f
Yurii Rochniak <yrochnyak@gmail.com>
2022-09-11 17:03:20 +0200

    Prepare for a new release

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 3.37"
+      version               = "~> 4"
       configuration_aliases = [aws.this, aws.peer]
     }
   }
