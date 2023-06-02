07a355f80d2c1e755b668a3bc606f0ccbfc155ba
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2023-06-02 10:53:43 +0200

    change condition

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5"
+      version               = ">= 5"
       configuration_aliases = [aws.this, aws.peer]
     }
   }
