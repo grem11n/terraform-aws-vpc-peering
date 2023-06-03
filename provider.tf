e0373650ba25073f808294972f0551aef9ae94d1
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2023-06-03 11:52:14 +0200

    Update provider.tf
    Co-authored-by: Mohsen Sadeghzade <techieforfun@protonmail.com>

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 5"
+      version               = ">= 4"
       configuration_aliases = [aws.this, aws.peer]
     }
   }
