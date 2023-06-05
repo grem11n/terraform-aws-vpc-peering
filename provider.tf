764ae14d043d34b77fe25036342237a4f01c0af2
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2023-06-05 14:53:47 +0200

    Update AWS provider version (#103)
    * update provider version
    
    * change condition
    
    * Update provider.tf
    
    Co-authored-by: Mohsen Sadeghzade <techieforfun@protonmail.com>
    
    ---------
    
    Co-authored-by: Mohsen Sadeghzade <techieforfun@protonmail.com>

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 4"
+      version               = ">= 4"
       configuration_aliases = [aws.this, aws.peer]
     }
   }
