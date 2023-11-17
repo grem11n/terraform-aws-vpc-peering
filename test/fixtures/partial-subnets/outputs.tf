5edc28d177b36d955fa092c818d1d0d515c3b468
Siim Aus <s.aus@tickmill.com>
2023-11-17 14:01:44 +0000

    Test fixture for partial-subnets

---
+++ test/fixtures/partial-subnets/outputs.tf
@@ -0,0 +1,31 @@
+output "this_vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "peer_vpc_id" {
+  value = aws_vpc.peer.id
+}
+
+output "this_main_route_table" {
+  value = data.aws_route_table.this_main_route_table.id
+}
+
+output "peer_main_route_table" {
+  value = data.aws_route_table.peer_main_route_table.id
+}
+
+output "this_subnet_ids" {
+  value = aws_subnet.this.*.id
+}
+
+output "peer_subnet_ids" {
+  value = aws_subnet.peer.*.id
+} 
+
+output "this_route_tables" {
+  value = aws_route_table.this.*.id
+}
+
+output "peer_route_tables" {
+  value = aws_route_table.peer.*.id
+}
