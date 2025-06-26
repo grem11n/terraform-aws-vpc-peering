55cc74ed5d9f8919d8036cd57f0568037e2b521a
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2025-06-26 17:50:56 +0200

    fixes the problem of creating peering with non-existent networks (#122)
    * fixes the problem of creating peering with non-existent networks
    
    * disable ipv6 by default

--- locals.tf
+++ locals.tf
@@ -10,7 +10,7 @@ locals {
   this_subnet_route_table_map = {
     for subnet in data.aws_subnets.this.ids :
     subnet => concat(
-      data.aws_route_tables.this_associated_route_tables[subnet].ids,
+      try(data.aws_route_tables.this_associated_route_tables[subnet].ids, []),
       [data.aws_route_table.this_main_route_table.id]
     )[0]
   }
@@ -18,30 +18,30 @@ locals {
   peer_subnet_route_table_map = {
     for subnet in data.aws_subnets.peer.ids :
     subnet => concat(
-      data.aws_route_tables.peer_associated_route_tables[subnet].ids,
+      try(data.aws_route_tables.peer_associated_route_tables[subnet].ids, []),
       [data.aws_route_table.peer_main_route_table.id]
     )[0]
   }
 
-  this_rts_ids = length(var.this_subnets_ids) == 0 ? distinct(values(local.this_subnet_route_table_map)) : distinct([
+  this_rts_ids = length(var.this_subnets_ids) == 0 ? data.aws_route_tables.this_all_route_tables.ids : distinct([
     for subnet_id in var.this_subnets_ids : local.this_subnet_route_table_map[subnet_id]
   ])
 
-  peer_rts_ids = length(var.peer_subnets_ids) == 0 ? distinct(values(local.peer_subnet_route_table_map)) : distinct([
+  peer_rts_ids = length(var.peer_subnets_ids) == 0 ? data.aws_route_tables.peer_all_route_tables.ids : distinct([
     for subnet_id in var.peer_subnets_ids : local.peer_subnet_route_table_map[subnet_id]
   ])
 
   # `this_dest_cidrs` represent CIDR of peer VPC, therefore a destination CIDR for this_vpc
   # `peer_dest_cidrs` represent CIDR of this VPC, therefore a destination CIDR for peer_vpc
   # Destination cidrs for this are in peer and vice versa
-  this_dest_ipv4_cidrs = toset(compact(length(var.peer_subnets_ids) == 0 ? [data.aws_vpc.peer_vpc.cidr_block] : data.aws_subnet.peer[*].cidr_block))
-  this_dest_ipv6_cidrs = toset(compact(length(var.peer_subnets_ids) == 0 && var.use_ipv6 ? [data.aws_vpc.peer_vpc.ipv6_cidr_block] : data.aws_subnet.peer[*].ipv6_cidr_block))
-  peer_dest_ipv4_cidrs = toset(compact(length(var.this_subnets_ids) == 0 ? [data.aws_vpc.this_vpc.cidr_block] : data.aws_subnet.this[*].cidr_block))
-  peer_dest_ipv6_cidrs = toset(compact(length(var.this_subnets_ids) == 0 && var.use_ipv6 ? [data.aws_vpc.this_vpc.ipv6_cidr_block] : data.aws_subnet.this[*].ipv6_cidr_block))
+  this_dest_ipv4_cidrs = toset(length(var.peer_subnets_ids) == 0 ? [data.aws_vpc.peer_vpc.cidr_block] : data.aws_subnet.peer[*].cidr_block)
+  this_dest_ipv6_cidrs = toset(length(var.peer_subnets_ids) == 0 && var.use_ipv6 ? [data.aws_vpc.peer_vpc.ipv6_cidr_block] : data.aws_subnet.peer[*].ipv6_cidr_block)
+  peer_dest_ipv4_cidrs = toset(length(var.this_subnets_ids) == 0 ? [data.aws_vpc.this_vpc.cidr_block] : data.aws_subnet.this[*].cidr_block)
+  peer_dest_ipv6_cidrs = toset(length(var.this_subnets_ids) == 0 && var.use_ipv6 ? [data.aws_vpc.this_vpc.ipv6_cidr_block] : data.aws_subnet.this[*].ipv6_cidr_block)
 
   # Get associated CIDR blocks
-  this_associated_dest_cidrs = toset(compact([for k, v in data.aws_vpc.peer_vpc.cidr_block_associations : v.cidr_block]))
-  peer_associated_dest_cidrs = toset(compact([for k, v in data.aws_vpc.this_vpc.cidr_block_associations : v.cidr_block]))
+  this_associated_dest_cidrs = toset([for k, v in data.aws_vpc.peer_vpc.cidr_block_associations : v.cidr_block])
+  peer_associated_dest_cidrs = toset([for k, v in data.aws_vpc.this_vpc.cidr_block_associations : v.cidr_block])
 
   # Allow specifying route tables explicitly
   this_rts_ids_hack = length(var.this_rts_ids) == 0 ? local.this_rts_ids : var.this_rts_ids
