99eefabfef1651fb68a8721bde8eeddef30855a4
Yurii Rochniak <yrochnyak@gmail.com>
2025-08-20 20:10:37 +0200

    Require AWS provider >= 6 (#127)

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 4"
+      version               = ">= 6"
       configuration_aliases = [aws.this, aws.peer]
     }
   }
